e820df5964c8b9db9c9a3761cf36674daf7ba3cc
Ne4 <61q11w22@gmail.com>
2025-12-20 18:24:54 +0300

    Update files

--- js/random.js
+++ js/random.js
@@ -16,10 +16,9 @@ const value11 = document.getElementById("value11");
 // Кнопка запуска расчёта
 const start = document.getElementById("start");
 
-function getRandomValue(min, max) { // Возвращает случайное число кратное 5.
+function getRandomValue(min, max) { // Возвращает случайное число кратное 5 в указанном диапазоне.
     const minMultiple = Math.ceil(min/5) * 5;
     const maxMultiple = Math.ceil(max/5) * 5;
-    // return Math.floor(Math.random() * (maxMultiple - minMultiple)) + minMultiple;
     const count = (maxMultiple - minMultiple) / 5 + 1;
     return minMultiple + Math.floor(Math.random() * count) * 5;
 }
@@ -43,37 +42,61 @@ function admission() { // Выдаёт массив с допусками по �
     }
 }
 function getWeight() {
-    const value1 = document.getElementById("value1").value;
-    const value2 = document.getElementById("value2").value;
-    const value3 = document.getElementById("value3").value;
-    const value4 = document.getElementById("value4").value;
-    const value5 = document.getElementById("value5").value;
-    const value6 = document.getElementById("value6").value;
-    const value7 = document.getElementById("value7").value;
-    const value8 = document.getElementById("value8").value;
-    const value9 = document.getElementById("value9").value;
-    const value10 = document.getElementById("value10").value;
-    const value11 = document.getElementById("value11").value;
+    const value1 = parseInt(document.getElementById("value1").value);
+    const value2 = parseInt(document.getElementById("value2").value);
+    const value3 = parseInt(document.getElementById("value3").value);
+    const value4 = parseInt(document.getElementById("value4").value);
+    const value5 = parseInt(document.getElementById("value5").value);
+    const value6 = parseInt(document.getElementById("value6").value);
+    const value7 = parseInt(document.getElementById("value7").value);
+    const value8 = parseInt(document.getElementById("value8").value);
+    const value9 = parseInt(document.getElementById("value9").value);
+    const value10 = parseInt(document.getElementById("value10").value);
+    const value11 = parseInt(document.getElementById("value11").value);
     return [value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11]
 }
 
+function getMinMax() {
+    const values = [];
+    for (let i = 0; i < 11; i++) { // Расчитывает минимальное и максимальное значение для каждой ячейки
+            if (getWeight()[i] > 1000) {
+                values.push(getWeight()[i] - (getWeight()[i] * 0.10));
+                values.push(getWeight()[i] + (getWeight()[i] * 0.10));
+            }
+            else {
+                values.push(getWeight()[i] - (getWeight()[i] * 0.20));
+                values.push(getWeight()[i] + (getWeight()[i] * 0.20));
+            }
+    }
+
+    for (let i = 0; i < 11; i++){ // Меняет тип данных на Int
+        values[i] = parseInt(values[i]);
+    }
+return values
+}
+
 function getSample() {
-    
+    for (i=0; i <= 8; i++) { //Задаём рандомные значения
+        
+    }
+    // Добавь расчёт полных ПО и ПП
+    // Проверяем подходит ли значение по допускам
 }
 
 
 // 1. Задаём рандомное число +
-// 2. Задать список допустимых значений по гост +
-// 2. Проверяем какая у нас ЩГПС 
-// 3. Парсим значения из вставленного рассева 
-// 4. Определяем диапазон на сколько можно прибавлять или убавлять для каждой ячейки
-// 5. Прибавляем к каждой ячейке число кратное 5
-// 6. Проверка суммы полученного значения
-// 7. Выдаём требуемое количнство рассевов
+// 2.1 Проверяем какая у нас ЩГПС +
+// 2.2 Задать список допустимых значений по гост +
+// 3. Получаем значения из вставленного рассева  +
+// 4. Определяем диапазон на сколько можно прибавлять или убавлять +
+// 5. Создаём рандомное число в диапозоне (П.4) кратное 5
+// 6. Проверка суммы полученного значения (пока хз как реализовать)
+// 7. Выдаём требуемое количнство рассевов 
 
 function check() {
     console.log(admission());
     console.log(getWeight());
+    console.log(getMinMax())
 }
 
 
