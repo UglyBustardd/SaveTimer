b8600e37bfb9364e8d421ddb99cd5126acf4e7c1
Ne4 <61q11w22@gmail.com>
2026-01-17 02:30:22 +0300

    Start

--- js/random.js
+++ js/random.js
@@ -16,31 +16,32 @@ function getWeight() { // Получаем значения из примера 
     const value10 = parseInt(document.getElementById("value10").value);
     const value11 = parseInt(document.getElementById("value11").value);
     const value12 = parseInt(document.getElementById("value12").value);
-    return [value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11,value12]
+    const value13 = parseInt(document.getElementById("value13").value);
+    return [value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11,value12, value13]
 }
 function getAdmission() { // Выдаёт допуски в ПО
     const soilType = document.getElementById("soil").value;
     // В список включены ячейки, которые не учитываются в ГОСТ`е (просто продублировал предыдущие)
     if (soilType === "c1") {
-        return [0, 0, 0, 10, 20, 40, 20, 40, 35, 60, 45, 70, 55, 80, 55, 80, 70, 90, 75, 92, 80, 93, 100, 100]
+        return [0, 0, 0, 10, 20, 40, 20, 40, 35, 60, 45, 70, 55, 80, 55, 80, 70, 90, 70, 90, 75, 92, 80, 93, 100, 100]
     }
     else if (soilType === "c2") {
-        return [0, 0, 0, 0, 0, 10, 0, 10, 10, 35, 25, 50, 35, 65, 35, 65, 55, 80, 65, 90, 75, 92, 100, 100]
+        return [0, 0, 0, 0, 0, 10, 0, 10, 10, 35, 25, 50, 35, 65, 35, 65, 55, 80, 55, 80, 65, 90, 75, 92, 100, 100]
     }
     else if (soilType === "c4") {
-        return [0, 10, 15, 35, 28, 55, 28, 55, 40, 70, 50, 80, 60, 85, 60, 85, 80, 95, 91, 97, 95, 100, 100, 100]
+        return [0, 10, 15, 35, 28, 55, 28, 55, 40, 70, 50, 80, 60, 85, 60, 85, 80, 95, 80, 95, 91, 97, 95, 100, 100, 100]
     }
     else if (soilType === "c5") {
-        return [0, 0, 0, 10, 25, 60, 25, 60, 45, 80, 57, 85, 67, 88, 67, 88, 80, 95, 90, 97, 95, 100, 100, 100]
+        return [0, 0, 0, 10, 25, 60, 25, 60, 45, 80, 57, 85, 67, 88, 67, 88, 80, 95, 80, 95, 90, 97, 95, 100, 100, 100]
     }
     else if (soilType === "c10") {
-        return [0, 0, 0, 10, 25, 60, 25, 60, 45, 80, 57, 85, 57, 85, 57, 85, 71, 91, 87, 97, 95, 100, 100, 100]
+        return [0, 0, 0, 10, 25, 60, 25, 60, 45, 80, 57, 85, 57, 85, 57, 85, 71, 91, 71, 91, 87, 97, 95, 100, 100, 100]
     }
 }
 function calculatePP(weight) { // Расчёт ПП
     weight = getWeight();
     const PP = [];
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 13; i++) {
         PP.push(weight[i] / 100);
     }
     return PP
@@ -55,30 +56,42 @@ function calculatePO(PP) { // Расчёт ПО
     }
     return PO
 }
-function randomSieve(min, max) {
+function randomSieve(min, max, targetValue) {
     let random = Math.random() * (max - min) + min;
+    if ((Math.abs(random - targetValue)) > (targetValue * 0.2)) {
+        let modul = Math.random();
+        if (modul > 0.5) {
+        random = (targetValue + targetValue * Math.random()) * (max - min) + min;
+        }
+        else {
+            random = (targetValue - targetValue * Math.random()) * (max - min) + min;
+        }
+    }
+    else {
+    random = random;
+    }
     random = random - (random % 0.05);
     return parseFloat(random.toFixed(2))
 }
 
 function calculateRandomSieve() { // Возвращает ПО для рандомного рассева
     const target = []; // Значение ПО целевого рассева
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 13; i++) {
         target.push(calculatePO(calculatePP(getWeight()))[i])
     }
 
     const admissionMax = []; // Верх по допуску по ГОСТ
-    for (let i = 1; i < 24; i += 2) {
+    for (let i = 1; i < 26; i += 2) {
         admissionMax.push(getAdmission()[i]);
     }
 
     const admissionMin = []; // Низ по допуску по ГОСТ
-    for (let i = 0; i < 24; i += 2) {
+    for (let i = 0; i < 26; i += 2) {
         admissionMin.push(getAdmission()[i]);
     }
 
     const sieve = []; // Значения ПО для рандомного рассева
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 13; i++) {
         let minValue = admissionMin[i];
         if (i > 0) {
             minValue = Math.max(admissionMin[i], sieve[i - 1]);
@@ -87,30 +100,28 @@ function calculateRandomSieve() { // Возвращает ПО для рандо
             sieve.push(admissionMax[i]);
         }
         else {
-            sieve.push(randomSieve(minValue, admissionMax[i]))
+            sieve.push(randomSieve(minValue, admissionMax[i], target[i]))
         }
     }
-    sieve[11] = 100;
+    sieve[12] = 100;
     return sieve
 }
 
 function calculatePoToPp() {
     const randomPP = [];
     const randomPO = calculateRandomSieve();
-    // const randomPO = [1, 3, 5, 7, 9, 10, 17, 18, 29, 31, 51, 122];
     randomPP.push(randomPO[0]);
-    for (let i = 1; i < 12; i++) {
+    for (let i = 1; i < 13; i++) {
         let PoToPp = (randomPO[i] - randomPO[i-1]).toFixed(2);
         randomPP.push(PoToPp);
     }
-    // console.log(randomPO); // Проверка
     return randomPP
 }
 
 function calculatePpToWeight() {
     const randomWeight = [];
     const randomPP = calculatePoToPp();
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 13; i++) {
         let PoToPp = parseFloat(randomPP[i] * 100).toFixed(0);
         randomWeight.push(PoToPp)
     }
@@ -124,18 +135,18 @@ function calculatePpToWeight() {
     for (let i = 0; i < quantityOfSieve; i++) {
         const newDiv = document.createElement("div");
         newDiv.className = "randomSieve";
-        
+
         // Добавляем заголовок с порядковым номером
         const number = document.createElement("h2");
         number.textContent = `Рассев: ${i + 1}`;
         newDiv.append(number);
-        
+
         const table = document.createElement("table");
-        
-        // Создаем таблицу
+
+        // Создание таблицу
         for (let row = 1; row < 5; row++) {
             const tr = document.createElement("tr");
-            for (let col = 1; col < 13; col++) {
+            for (let col = 1; col < 14; col++) {
                 const td = document.createElement("td");
                 td.className = `cell_${row}_${col}`;
                 tr.appendChild(td);
@@ -144,26 +155,26 @@ function calculatePpToWeight() {
         }
         newDiv.append(table);
         main.append(newDiv);
-        
-        // Заполнение данными (используем селекторы внутри newDiv)
-        const admissionValues = [70, 40, 20, 15, 10, 5, 2.5, 1.25, 0.63, 0.16, 0.05, "<"];
-        
-        for (let j = 0; j < 12; j++) {
+
+        // Заполнение данными
+        const admissionValues = [70, 40, 20, 15, 10, 5, 2.5, 1.25, 0.63, 0.315, 0.16, 0.05, "<"];
+
+        for (let j = 0; j < 13; j++) {
             const valueOfSieve = newDiv.querySelector(`.cell_1_${j+1}`);
             valueOfSieve.textContent = admissionValues[j];
         }
-        
-        for (let j = 0; j < 12; j++) {
+
+        for (let j = 0; j < 13; j++) {
             let valueOfWeight = newDiv.querySelector(`.cell_2_${j+1}`);
             valueOfWeight.textContent = calculatePpToWeight()[j];
         }
-        
-        for (let j = 0; j < 12; j++) {
+
+        for (let j = 0; j < 13; j++) {
             let valueOfPP = newDiv.querySelector(`.cell_3_${j+1}`);
             valueOfPP.textContent = calculatePoToPp()[j];
         }
-        
-        for (let j = 0; j < 12; j++) {
+
+        for (let j = 0; j < 13; j++) {
             let valueOfPO = newDiv.querySelector(`.cell_4_${j+1}`);
             valueOfPO.textContent = calculateRandomSieve()[j];
         }
